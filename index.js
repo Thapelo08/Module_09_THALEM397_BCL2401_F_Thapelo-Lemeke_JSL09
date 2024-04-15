f833d47307fd1625dd5b6e966cbf22fed4e14d0e
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-04-15 10:25:20 +0200

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ try {
     document.getElementById("author").textContent = `By: ${data.user.name}`;
 } catch (err) {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
-    document.getElementById("author").textContent = `By: Dodi Achmad`;
+    document.getElementById("author").textContent = `By: Thapel Lemeke`;
 }
 try {
     const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
